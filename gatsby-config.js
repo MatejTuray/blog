5cbc0f982ac5500a2b1935f60beb90017efce2f1
MatejTuray <matej.turay@gmail.com>
2019-04-15 20:39:02 +0200

    post

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/favicon.png`,
       },
     },
     `gatsby-plugin-offline`,
